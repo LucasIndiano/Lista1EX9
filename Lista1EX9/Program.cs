f768b7aeb64e841bee38656467793f604425f81f
ALUNO <ALUNO@LABFPC34>
2024-02-26 08:58:20 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1EX9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Digite um Número: ");
+            a = int.Parse(Console.ReadLine());
+            int rest = a % 2;
+            if (rest == 0) { Console.WriteLine("O Numero é Par"); }
+            else { Console.WriteLine("O Número é Impar"); }
+
+            Console.ReadKey();
+        }
+    }
+}
